dca8017dcf55c0e8064d87901306d74017df890d
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-19 15:47:49 +0530

    Welcome to stock management system

---
+++ src/com/stockmanagement/StockManagement.java
@@ -0,0 +1,7 @@
+package com.stockmanagement;
+
+public class StockManagement {
+    public static void main(String[] args) {
+        System.out.println("*********** Welcome Tob Stock Management System *******************");
+    }
+}
